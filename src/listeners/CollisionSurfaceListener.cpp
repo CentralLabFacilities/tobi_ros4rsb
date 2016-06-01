89d18d2d3c97714a56c6152d0bfff2385d261288
Nils Neumann <nneumann@techfak.uni-bielefeld.de>
2016-06-01 16:50:53 +0200

    remove old workaround

--- src/listeners/CollisionSurfaceListener.cpp
+++ src/listeners/CollisionSurfaceListener.cpp
@@ -110,7 +110,7 @@ void CollisionSurfaceListener::callback(PatchesPtr input) {
         geometry_msgs::PoseStamped poseOld;
         poseOld.pose.position.x = patch.base().translation().x();
         poseOld.pose.position.y = patch.base().translation().y();
-        poseOld.pose.position.z = patch.base().translation().z() - ( primitive.dimensions[2] / 2);
+        poseOld.pose.position.z = patch.base().translation().z();
         poseOld.pose.orientation.w = patch.base().rotation().qw();
         poseOld.pose.orientation.x = patch.base().rotation().qx();
         poseOld.pose.orientation.y = patch.base().rotation().qy();
