50ff694dc65bb749728e37b5f46b2309031cd407
Luca Lach <llach@techfak.uni-bielefeld.de>
2016-06-12 15:51:31 +0200

    publishing correct plane

--- src/listeners/CollisionSurfaceListener.cpp
+++ src/listeners/CollisionSurfaceListener.cpp
@@ -216,7 +216,7 @@ void CollisionSurfaceListener::callback(PatchesPtr input) {
     surfaceUpper.primitive_poses[0].position.y = surfaceHigh.primitive_poses[0].position.y;
     surfaceUpper.primitive_poses[0].position.z = surfaceHigh.primitive_poses[0].position.z + 0.34;
 
-    surfaces.push_back(surfaceHigh);
+    surfaces.push_back(surfaceUpper);
 
     sceneInterface.addCollisionObjects(surfaces);
 }
