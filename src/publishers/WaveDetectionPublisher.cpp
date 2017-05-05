8e36fec017e4eae66516bf452e2fca96738df257
llach <llach@techfak.uni-bielefeld.de>
2017-05-05 19:59:26 +0200

    trying to fix git reset

--- src/publishers/WaveDetectionPublisher.cpp
+++ src/publishers/WaveDetectionPublisher.cpp
@@ -1,7 +1,7 @@
-/* 
+/*
  * File:   WaveDetectionPublisher.cpp
  * Author: alangfel
- * 
+ *
  * Created on April 25, 2014, 2:37 PM
  */
 
@@ -15,13 +15,13 @@ namespace ros4rsb {
 
     WaveDetectionPublisher::WaveDetectionPublisher(const std::string &topicIn,const std::string &name, ros::NodeHandle &node) : PublisherImpl(name, node) {
         boost::function<void(const waving_detection::HandCreateConstPtr&) > m0 =
-                bind(mem_fn(&WaveDetectionPublisher::handCreateCallback), this, _1);
+                                                                                    bind(mem_fn(&WaveDetectionPublisher::handCreateCallback), this, _1);
         handCreateSub = node.subscribe("/HandCreate", 1000, m0);
         boost::function<void(const waving_detection::HandDestroyConstPtr&) > m1 =
-                bind(mem_fn(&WaveDetectionPublisher::handDestroyCallback), this, _1);
+                                                                                     bind(mem_fn(&WaveDetectionPublisher::handDestroyCallback), this, _1);
         handDestroySub = node.subscribe("/HandDestroy", 1000, m1);
         boost::function<void(const waving_detection::HandUpdateConstPtr&) > m2 =
-                bind(mem_fn(&WaveDetectionPublisher::handUpdateCallback), this, _1);
+                                                                                    bind(mem_fn(&WaveDetectionPublisher::handUpdateCallback), this, _1);
         handUpdateSub = node.subscribe("/HandUpdate", 1000, m2);
         tfListener = new tf::TransformListener(node);
 
@@ -33,7 +33,7 @@ namespace ros4rsb {
     }
 
     void WaveDetectionPublisher::handDestroyCallback(const waving_detection::HandDestroyConstPtr& msg) {
-        //transformAndPublish(msg->header.stamp.toNSec());    
+        //transformAndPublish(msg->header.stamp.toNSec());
     }
 
     void WaveDetectionPublisher::handUpdateCallback(const waving_detection::HandUpdateConstPtr& msg) {
@@ -45,21 +45,21 @@ namespace ros4rsb {
         geometry_msgs::PointStamped pointIn;
         // Robot's x is Kinect's z and robot's y is Kinect's x
         //TODO change datatype to rst
-        
+
         shared_ptr<geometry::Pose> data = shared_ptr<geometry::Pose>(new geometry::Pose());
-        
+
         data->mutable_rotation()->set_qx(0);
         data->mutable_rotation()->set_qy(0);
         data->mutable_rotation()->set_qz(0);
         data->mutable_rotation()->set_qw(0);
-        
-        
+
+
         data->mutable_translation()->set_x(z/1000.0);
         data->mutable_translation()->set_y(-x/1000.0);
         data->mutable_translation()->set_z(y/1000.0);
 
         rsb::EventPtr event = rsbInformer->createEvent();
-		event->setData(data);
-		this->publish(event);
+        event->setData(data);
+        this->publish(event);
     }
 }
